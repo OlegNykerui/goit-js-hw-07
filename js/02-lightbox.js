9090151072ff0b8d260e95e4ffb6f4a45fb3cbde
Oleg Nykerui <nykeruyoleg96@gmail.com>
2022-08-10 01:40:11 +0300

    add 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+let refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+// refs.gallery.addEventListener("click", onImageClick);
+
+refs.gallery.insertAdjacentHTML("beforeend", loadGallery());
+
+function loadGallery() {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        ` <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a> 
+
+`
+    )
+    .join("");
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+  close: false,
+  showCounter: false,
+});
+
+// let lightbox1111 = new SimpleLightbox(".gallery a", {
+//   captionsData: "alt",
+//   captionDelay: 250,
+//   captionPosition: "bottom",
+// });
